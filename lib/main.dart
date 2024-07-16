53c0d0090853d7b180c1c3567d4b0076e26c894b
Hitesh <Hiteshkhatri910>
2024-07-17 01:27:48 +0530

    WallpaperApp

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/utils/app_routes.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      routes: app_routes,
+    )
+  );
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/screen/home/view/home_screen.dart';
+
+Map<String,WidgetBuilder> app_routes = {
+  "/":(context) => HomeScreen(),
+};
